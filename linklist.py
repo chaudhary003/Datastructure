d54c85655c964429eb596d64ffc06b73b7622842
arvind chaudhary <chaudhary003@gmail.com>
2019-05-23 18:20:20 +0530

    updated till deque remaining postional list

--- linklist.py
+++ linklist.py
@@ -112,6 +112,112 @@ class LiknedQueue:
                 self._tail=None
             return result
 
+class CircularQueue:
+    '''class repesenting a circular queue using linkedlist'''
+    class _Node:
+        '''Light weight class representing node objects in memory'''
+        __slot__='_next','_data'
+        def __init__(self,data,next):
+            self._next=next
+            self._data=data
+    def __init__(self):
+        self._tail=None
+        self._size=0
+    def __len__(self):
+        return self._size
+    def is_empty(self):
+        return self._size==0
+    def first(self):
+        '''return first element of the queue without removing it'''
+        if self.is_empty(self):
+            raise Empty('queue is empty')
+        firstElement=self._tail._next
+        return firstElement._data
+    def dequeue(self):
+        '''return and remove first element of the queue'''
+        if self.is_empty():
+            raise Empty('queue is empty')
+        head=self._tail._next
+        if self._size==1:
+            self._tail=None
+        else:
+            self._tail._next=head._next
+            self._size -=1
+        return head
+    def enqueue(self):
+        ''' add an elemnt at the end of queue'''
+        newNode=self._Node(data,None)
+        if self.is_empty(self):
+            newNode._next=newNode
+            self._size +=1
+        else:
+            newNode._next=self._tail._next
+            self._tail._next=newNode
+        self._tail=newNode
+        size +=1
+    def rotate(self):
+        if self._size >0:
+            self._tail=self._tail._next
+
+class  _DoublyLinkedListBase:
+    ''' base class representating doubly linked list '''
+    def _Node:
+        __slot__='_data','_next','_previous'
+        def __init__(self,data,next,previous):
+            self._data=data
+            self._next=next
+            self._previous=previous
+    def __init__(self):
+        self._frontEnd=self._Node(None,None,None)
+        self._rareEnd=self._Node(None,None,None)
+        self._frontEnd._next=self._rareEnd
+        self._rareEnd._previous=self._frontEnd
+        self._size=0
+    def __len__(self):
+        return self._size
+    def is_empty(self):
+        return self._size==0
+    def _insertBetween(self,data,predecessor,successor):
+        newNode=self._Node(data,predecessor,successor)
+        predecessor._next=newNode
+        successor._previous=newNode
+        size +=1
+        return newNode
+    def _deleteNode(self,node):
+        predecessor=node._previous
+        successor=node._next
+        predecessor._next=successor
+        successor._previous=predecessor
+        element=node._data
+        node._previous=Node._next=node._data=None
+        size -=1
+        return element
+''' double ended queue  using linkedlist'''
+class Deque(_DoublyLinkedListBase):
+    def firstElement(self):
+        if self.is_empty():
+            raise Empty('deque is empty')
+            return self._frontEnd._next._data
+    def lastElement(self):
+        if self.is_empty():
+            raise Empty('deque is empty')
+        return self._rareEnd._previous._data
+    def addFirst(self,data):
+        ''' add element at the front end of the deque'''
+        self._insertBetween(data,self._frontEnd,self._frontEnd._next)
+    def deleteFirst(self):
+        ''' delete a node from the deque and return node data''''
+        if self.is_empty():
+            raise Empty('deque is empty')
+        return self._deleteNode(self._frontEnd._next)
+    def deleteLast(self):
+        if self.is_empty():
+            raise Empty('deque is empty')
+        return self._deleteNode(self._rareEnd._previous)
+
+
+
+
 
 
 
